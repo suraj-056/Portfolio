902d15674ef03af30978bfae0fc4a8e554ebb420
SURAJ <77103955+suru77@users.noreply.github.com>
2024-01-05 20:22:09 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+// Get the button:
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+
+
+
+
+
+// testing//
+
+// in this demo, it is triggered by a click event
+// you may use any trigger in your apps
+
+
+
+document.getElementById('one')
+
+onclick="location.href='https://drive.google.com/file/d/1bh1kV8jmA55thG5fzW3h8nRtNWQkZYyf/view?usp=drive_link';"
